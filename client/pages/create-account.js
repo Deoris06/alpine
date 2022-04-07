64688524e341001b00065f2bde78a02c17d96470
Deoris06 <adekschris@gmail.com>
2022-04-07 20:45:35 +0100

    changed the focus input box

--- client/pages/create-account.js
+++ client/pages/create-account.js
@@ -63,7 +63,7 @@ const CreateAccount = () => {
                 onChange={handleChange('first_name')}
                 placeholder="Enter your First name"
                 type="text" 
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                className="block w-full px-4 py-2 mt-2 text-gray-700 ring-1 ring-slate-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-yellow-400 focus:ring-yellow-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
             </div>
             <div>
                 <label className="text-gray-700 dark:text-gray-200" for="last_name">Last name</label>
@@ -72,7 +72,7 @@ const CreateAccount = () => {
                 onChange={handleChange('last_name')}
                 placeholder="Enter your Last name"
                 type="text" 
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                className="block w-full px-4 py-2 mt-2 text-gray-700 ring-1 ring-slate-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-yellow-400 focus:ring-yellow-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
             </div>
             <div>
                 <label className="text-gray-700 dark:text-gray-200" for="emailAddress">Email Address</label>
@@ -80,7 +80,7 @@ const CreateAccount = () => {
                 value={email}
                 onChange={handleChange('email')}
                 placeholder="Enter your email"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                className="block w-full px-4 py-2 mt-2 text-gray-700 ring-1 ring-slate-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-yellow-400 focus:ring-yellow-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
             </div>
 
             <div>
@@ -90,7 +90,7 @@ const CreateAccount = () => {
                 onChange={handleChange('password')}
                 placeholder="Enter password"
                 type="password" 
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                className="block w-full px-4 py-2 mt-2 text-gray-700 ring-1 ring-slate-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-yellow-400 focus:ring-yellow-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
             </div>
 
             <div>
@@ -100,7 +100,7 @@ const CreateAccount = () => {
                 onChange={handleChange('c_password')}
                 placeholder="Confirm password" 
                 type="password" 
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                className="block w-full px-4 py-2 mt-2 text-gray-700 ring-1 ring-slate-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-yellow-400 focus:ring-yellow-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
             </div>
         </div>
         <div className='flex justify-between mt-8'>
@@ -108,7 +108,7 @@ const CreateAccount = () => {
                 {buttonText}
             </button>
             <Link
-            href="/"
+            href="/login"
             >
             <a  className=" self-center font-semibold text-gray-600 text-lg underline hover:text-amber-400">
                 Have an account? Login
